c0265cb11372a8de68d79418faa90439c14ba4bc
shingo-cc <shingo-cc>
2026-06-22 15:46:35 +0900

    fix(migration): register inventory aggregation rules

--- scripts/run_all_migrations.sh
+++ scripts/run_all_migrations.sh
@@ -167,6 +167,7 @@ run_sql migrations/081_create_inventory.sql
 run_sql migrations/082_extend_products_box_attributes.sql
 run_sql migrations/083_add_staff_phone.sql
 run_sql migrations/084_add_unit_to_inventory.sql
+run_sql migrations/20260620_010000_create_inventory_aggregation_rules.sql
 run_sql migrations/20260622_020000_add_inventory_raw_condition.sql
 run_sql migrations/085_create_tcg_type_master.sql
 run_sql migrations/086_seed_additional_tcg_types.sql
