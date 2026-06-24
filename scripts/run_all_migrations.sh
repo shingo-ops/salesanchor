5f8d7097c964d818142d597338972a6f8e08a673
shingo-cc <shingo-cc>
2026-06-24 13:28:28 +0900

    fix(migration): rename tcg_type_fk to 20260623_060000 to avoid timestamp collision with _drop_inventory_offer_key (20260623_030000)

--- scripts/run_all_migrations.sh
+++ scripts/run_all_migrations.sh
@@ -445,7 +445,7 @@ run_py  scripts/migrate_20260621_020000_backfill_lead_country.py
 run_sql migrations/20260623_100000_rls_message_translations.sql
 
 # SSOT cleanup 2: products.tcg_type を tcg_type_master.code に固定
-run_sql migrations/20260623_030000_add_products_tcg_type_fk.sql
+run_sql migrations/20260623_060000_add_products_tcg_type_fk.sql
 
 # 送信ガード土台: meta_messages.original_language を message_translations から backfill
 run_sql migrations/20260624_120000_backfill_meta_messages_original_language.sql
