a3260b5e4ea88e001d791770fdc982aa97efc7cb
shingo-cc <shingo-cc>
2026-06-25 19:16:22 +0900

    chore: remove planted color violation (KPI-4 red proof obtained)

--- frontend/src/pages/companies/PlantedColorViolation.tsx
+++
@@ -1,8 +0,0 @@
-// planted: CI赤確認用（証拠取得後に削除）
-export function PlantedColorViolation() {
-  return (
-    <div style={{ color: "#ff0000" }}>
-      planted color violation for KPI-4 proof
-    </div>
-  );
-}
